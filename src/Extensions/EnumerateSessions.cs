6510ce06127985e4ccbbc51f32c9206c70a41daf
bruno-f-cruz <bfpcruz@gmail.com>
2023-09-19 09:35:46 -0700

    Allow dynamic subject search

--- src/Extensions/EnumerateSessions.cs
+++ src/Extensions/EnumerateSessions.cs
@@ -7,6 +7,23 @@ using System.Reactive.Linq;
 
 public class EnumerateSessions: Source<string>
 {
+
+    private FileTypes fileFilter = FileTypes.YAML;
+    public FileTypes FileFilter
+    {
+        get { return fileFilter; }
+        set { fileFilter = value; }
+    }
+
+    private string directory = ".";
+    [Editor("Bonsai.Design.FolderNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+    [Description("The relative or absolute path of the selected folder.")]
+    public string Directory
+    {
+        get { return directory; }
+        set { directory = value; }
+    }
+
     private string subject;
     [TypeConverter(typeof(EnumeratedSubjectsConverter))]
     public string Subject
@@ -15,23 +32,30 @@ public class EnumerateSessions: Source<string>
         set { subject = value; }
     }
 
+    private static string BuildFullPath(string dir, string file){
+        return Path.Combine(dir, file);
+    }
+
     public override IObservable<string> Generate()
     {
-        return Observable.Return(Path.Combine(new EnumeratedSubjectsConverter().directory, subject));
+        return Observable.Return(BuildFullPath(Directory, Subject));
     }
+
     public IObservable<string> Generate<TSource>(IObservable<TSource> source)
     {
-        return source.Select(x => {return Path.Combine(new EnumeratedSubjectsConverter().directory, subject);});
+        return source.Select(x => {
+            return Path.Combine(BuildFullPath(Directory, Subject));
+            }
+            );
     }
 
     class EnumeratedSubjectsConverter : StringConverter
     {
-        public readonly string directory = "../local";
-        public readonly string fileFilter = "*.yml";
-
         public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
         {
-            var subjectFiles = Directory.GetFiles(directory, fileFilter).ToList();
+            EnumerateSessions outer = (EnumerateSessions) context.Instance;
+            var filterString = getFilterString(outer.FileFilter);
+            var subjectFiles = System.IO.Directory.GetFiles(outer.Directory, filterString).ToList();
             var filenames = subjectFiles.Select(x => Path.GetFileName(x)).ToList();
 
             return new StandardValuesCollection(filenames);
@@ -42,4 +66,26 @@ public class EnumerateSessions: Source<string>
             return true;
         }
     }
+
+    private static string getFilterString(FileTypes filter)
+    {
+        switch (filter)
+        {
+            case FileTypes.YAML:
+                return "*.yaml";
+            case FileTypes.YML:
+                return "*.yml";
+            case FileTypes.JSON:
+                return "*.json";
+            default:
+                throw new Exception("Unknown file type.");
+        }
+    }
+    public enum FileTypes
+    {
+        YAML,
+        YML,
+        JSON,
+    }
+
 }
