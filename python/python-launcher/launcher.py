97b2b62f00b575a362674c8c4941f33b5a3530c9
bruno-f-cruz <bfpcruz@gmail.com>
2023-12-21 08:51:09 -0800

    Run black

--- python/python-launcher/launcher.py
+++ python/python-launcher/launcher.py
@@ -1,22 +1,22 @@
 import subprocess
 import os
 
-subject = '672102'
-rig = 'rig'
-tasklogic = '672102'
+subject = "672102"
+rig = "rig"
+tasklogic = "672102"
 
 CWD = os.getcwd()
-LOCAL = os.path.join(CWD, 'local')
-BONSAI_EXE = os.path.join(CWD, 'Bonsai\\Bonsai.exe')
+LOCAL = os.path.join(CWD, "local")
+BONSAI_EXE = os.path.join(CWD, "Bonsai\\Bonsai.exe")
 
 ADD_FLAGS = "--no-editor"
 FORCE_LAYOUT = True
 SAVE_LOG = False
 
 Settings = {
-    "TaskLogicSchemaPath": os.path.join(LOCAL, 'TaskLogic', tasklogic + '.yml'),
-    "SubjectSchemaPath": os.path.join(LOCAL, 'Subjects', subject + '.yml'),
-    "RigSchemaPath": os.path.join(LOCAL, 'Rigs', rig + '.yml'),
+    "TaskLogicSchemaPath": os.path.join(LOCAL, "TaskLogic", tasklogic + ".yml"),
+    "SubjectSchemaPath": os.path.join(LOCAL, "Subjects", subject + ".yml"),
+    "RigSchemaPath": os.path.join(LOCAL, "Rigs", rig + ".yml"),
 }
 
 layout = os.path.join(CWD, "src\\vr-foraging_template.bonsai.layout")
@@ -27,20 +27,21 @@ output_cmd = f'"{BONSAI_EXE}" "{workflow}" {ADD_FLAGS}'
 for sett in Settings.keys():
     output_cmd += f' -p:"{sett}"="{Settings[sett]}"'
 
-if not(layout == "") and FORCE_LAYOUT:
+if not (layout == "") and FORCE_LAYOUT:
     output_cmd += f' --visualizer-layout:"{layout}"'
 
 print(f"Starting... {output_cmd}")
 
 if SAVE_LOG:
-    log = open(os.path.join(CWD + 'log.txt'), 'a')
+    log = open(os.path.join(CWD + "log.txt"), "a")
     bonsai_process = subprocess.Popen(
         output_cmd,
         cwd=CWD,
         creationflags=subprocess.CREATE_NEW_CONSOLE,
-        stdout=log, stderr=log)
+        stdout=log,
+        stderr=log,
+    )
 else:
     bonsai_process = subprocess.Popen(
-        output_cmd,
-        cwd=CWD,
-        creationflags=subprocess.CREATE_NEW_CONSOLE)
+        output_cmd, cwd=CWD, creationflags=subprocess.CREATE_NEW_CONSOLE
+    )
