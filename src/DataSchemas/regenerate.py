e1d05780e176e4c2b03e1d536c7f9c120c8c47bd
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-15 21:18:28 -0800

    Add Root class to allow arbitrary reference injection

--- src/DataSchemas/regenerate.py
+++ src/DataSchemas/regenerate.py
@@ -1,10 +1,10 @@
 import json
 from pathlib import Path
 
+import aind_behavior_vr_foraging.task_logic
 from aind_behavior_rig.base.json_schema import export_schema
 from aind_behavior_rig.utils import BonsaiSgenSerializers, bonsai_sgen, snake_to_pascale_case
 from aind_behavior_vr_foraging.session import AindVrForagingSession
-from aind_behavior_vr_foraging.task_logic import TaskLogic
 from pydantic.json_schema import GenerateJsonSchema
 
 SCHEMA_ROOT = Path("./src/DataSchemas/")
@@ -14,7 +14,10 @@ SGEN_SERIALIZERS = [BonsaiSgenSerializers.JSON, BonsaiSgenSerializers.YAML]
 
 
 def main():
-    models = {"aind_vr_foraging_task": TaskLogic, "aind_vr_foraging_session": AindVrForagingSession}
+    models = {
+        "aind_vr_foraging_task": aind_behavior_vr_foraging.task_logic.Root,
+        "aind_vr_foraging_session": AindVrForagingSession,
+    }
     for output_model_name, model in models.items():
         with open(SCHEMA_ROOT / f"{output_model_name}.json", "w") as f:
             json_model = json.dumps(export_schema(model), indent=2)
