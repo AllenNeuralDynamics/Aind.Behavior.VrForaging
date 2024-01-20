41d2f6f12f44d2ff7f772118fb7b78d4dd7387ad
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-20 06:48:17 -0800

    Add pydantic default keyword to most fields

--- src/DataSchemas/aind_behavior_vr_foraging/session.py
+++ src/DataSchemas/aind_behavior_vr_foraging/session.py
@@ -17,15 +17,15 @@ class Metadata(AindModel):
     experiment: str = Field(..., description="Name of the experiment")
     root_path: str = Field(..., description="Root path where data will be logged")
     remote_path: Optional[str] = Field(
-        None, description="Remote path where data will be attempted to be copied to after experiment is done"
+        default=None, description="Remote path where data will be attempted to be copied to after experiment is done"
     )
     subject: str = Field(..., description="Name of the subject")
     version: str = Field(..., description="Version of the experiment")
-    rng_seed: Optional[float] = Field(None, description="Seed of the random number generator")
-    notes: Optional[str] = Field(None, description="Notes about the experiment")
-    commit_hash: Optional[str] = Field(None, description="Commit hash of the repository")
-    allow_dirty_repo: bool = Field(False, description="Allow running from a dirty repository")
-    skip_hardware_validation: bool = Field(False, description="Skip hardware validation")
+    rng_seed: Optional[float] = Field(default=None, description="Seed of the random number generator")
+    notes: Optional[str] = Field(default=None, description="Notes about the experiment")
+    commit_hash: Optional[str] = Field(default=None, description="Commit hash of the repository")
+    allow_dirty_repo: bool = Field(default=False, description="Allow running from a dirty repository")
+    skip_hardware_validation: bool = Field(default=False, description="Skip hardware validation")
 
 
 class AindVrForagingSession(AindModel):
