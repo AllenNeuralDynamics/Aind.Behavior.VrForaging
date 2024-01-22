d20d7248f06f21e9ab00288ca9bd2a4e36106105
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-22 10:25:16 -0800

    Fix launcher behavior

--- launcher.py
+++ launcher.py
@@ -1,7 +1,5 @@
 import os
 import glob
-import json
-from typing import Literal
 import git
 
 from aind_behavior_vr_foraging import __version__
@@ -11,13 +9,14 @@ from aind_behavior_vr_foraging.session import AindVrForagingSession
 from pydantic import ValidationError, BaseModel
 import secrets
 
+ALLOW_DIRTY = False
 CWD = os.getcwd()
 CONFIG_LIBRARY = r"C:\git\AllenNeuralDynamics\aind-vr-foraging\local"
 BONSAI_EXE = "bonsai/bonsai.exe"
-WORKFLOW_FILE = "src/aind_vr_foraging.bonsai"
+WORKFLOW_FILE = "src\\vr-foraging.bonsai"
 COMPUTER_NAME = os.environ["COMPUTERNAME"]
-ROOT_DATA_PATH = "c:/data"
-REMOTE_DATA_PATH = "c:/data/remote"
+ROOT_DATA_PATH = "c:\\data"
+REMOTE_DATA_PATH = "c:\\data\\remote"
 TEMP = "local/temp"
 
 REPO = None
@@ -38,10 +37,11 @@ def _assert_dependencies():
         raise FileNotFoundError(f"Bonsai executable (bonsai.exe) not found! Expected {BONSAI_EXE}.")
     if not (os.path.isdir(CONFIG_LIBRARY)):
         raise FileNotFoundError(f"Config library not found! Expected {CONFIG_LIBRARY}.")
-    if not (os.path.isdir(os.path.join(CONFIG_LIBRARY, "rig", COMPUTER_NAME))):
+    if not (os.path.isdir(os.path.join(CONFIG_LIBRARY, "Rigs", COMPUTER_NAME))):
         raise FileNotFoundError(
-            f"Rig configuration not found! Expected {os.path.join(CONFIG_LIBRARY, 'rig', COMPUTER_NAME)}."
+            f"Rig configuration not found! Expected {os.path.join(CONFIG_LIBRARY, 'Rigs', COMPUTER_NAME)}."
         )
+    print(os.path.join(CWD, WORKFLOW_FILE))
     if not (os.path.isfile(os.path.join(CWD, WORKFLOW_FILE))):
         raise FileNotFoundError(f"Bonsai workflow file not found! Expected {WORKFLOW_FILE}.")
 
@@ -49,6 +49,8 @@ def _assert_dependencies():
 from os import PathLike
 from typing import Optional, Dict
 import subprocess
+from pydantic import BaseModel
+import json
 
 
 def run_bonsai_process(
@@ -77,16 +79,20 @@ def run_bonsai_process(
 
 def save_temp_model(model: BaseModel, folder: str = TEMP) -> PathLike:
     os.makedirs(folder, exist_ok=True)
-
     fname = model.__class__.__name__ + "_" + secrets.token_hex(nbytes=16) + ".json"
     with open(os.path.join(folder, fname), "w", encoding="utf-8") as f:
         f.write(model.model_dump_json(indent=3))
     return fname
 
 
+def load_json_model(json_path: PathLike, model: BaseModel) -> BaseModel:
+    with open(json_path, 'r', encoding="utf-8") as file:
+        return model.model_validate_json(file.read())
+
+
 def prompt_yes_no_question(question: str) -> bool:
     while True:
-        reply = input(question + " (Y/N): ").upper()
+        reply = input(question + " (Y\n): ").upper()
         if reply == "Y":
             return True
         elif reply == "N":
@@ -95,24 +101,30 @@ def prompt_yes_no_question(question: str) -> bool:
             print("Invalid input. Please enter 'Y' or 'N'.")
 
 
+def pick_file_from_list(available_files: list[str], prompt: str = "Choose a file:\n") -> str:
+    print(prompt)
+    print("0: Enter manually")
+    [print(f"{i+1}: {os.path.split(file)[1]}") for i, file in enumerate(available_files)]
+    choice = int(input("Choice: "))
+    if choice < 0 or choice >= len(available_files) + 1:
+        raise ValueError
+    if choice == 0:
+        path = str(input("Enter path: "))
+        return path
+    else:
+        return available_files[choice - 1]
+
+
 def prompt_task_logic_input(folder: str = "TaskLogic") -> AindVrForagingTaskLogic:
     available_files = glob.glob(os.path.join(CONFIG_LIBRARY, folder, "*.json"))
-    print("Choose a file:/n")
-    print("0: Manually enter path.")
-    [print(f"{i+1}: {os.path.split(file)[1]}") for i, file in enumerate(available_files)]
     while True:
         try:
-            choice = int(input("Choice: "))
-            if choice < 0 or choice >= len(available_files) + 1:
-                raise ValueError
-            if choice == 0:
-                path = str(input("Enter path:\n"))
-                task_logic = AindVrForagingTaskLogic.model_validate_json(path)
-            else:
-                print(f"Using {available_files[choice-1]}.")
-                task_logic = AindVrForagingTaskLogic.model_validate_json(available_files[choice - 1])
+            path = pick_file_from_list(available_files)
+            task_logic = load_json_model(path, AindVrForagingTaskLogic)
+            print(f"Using {path}.")
             return task_logic
-        except ValidationError:
+        except ValidationError as validation_error:
+            print(validation_error)
             print("Failed to validate pydantic model. Try again.")
         except ValueError:
             print("Invalid choice. Try again.")
@@ -127,43 +139,37 @@ def prompt_session_input(root_path: str = ROOT_DATA_PATH, remote_path: str = REM
             session = AindVrForagingSession(
                 experiment="aind_vr_foraging",
                 root_path=root_path,
-                remote_path=REMOTE_DATA_PATH,
+                remote_path=remote_path,
                 version=__version__,
                 subject=subject,
                 notes=notes,
                 commit_hash=REPO.head.commit.hexsha,
+                allow_dirty_repo=ALLOW_DIRTY
             )
             return session
         except ValidationError as e:
             print(f"Failed to validate pydantic model: {e}. Try again.")
 
 
-def prompt_rig_input(folder_name: str = "rig") -> AindVrForagingRig:
+def prompt_rig_input(folder_name: str = "Rigs") -> AindVrForagingRig:
     rig_schemas_path = os.path.join(CONFIG_LIBRARY, folder_name, COMPUTER_NAME)
+    print(rig_schemas_path)
     available_rigs = glob.glob(os.path.join(rig_schemas_path, "*.json"))
-    while True:
-        try:
-            if len(available_rigs) == 1:
-                print(f"Found a single rig config file. Using {available_rigs[0]}.")
-                return AindVrForagingRig.model_validate_json(available_rigs[0])
-            else:
-                print("Choose a file:/n")
-                print("0: Manually enter path.")
-                [print(f"{i+1}: {os.path.split(file)[1]}") for i, file in enumerate(available_rigs)]
-                choice = int(input("Choice: "))
-                if choice < 0 or choice >= len(available_rigs) + 1:
-                    raise ValueError
-                if choice == 0:
-                    path = str(input("Enter path:\n"))
-                    rig = AindVrForagingRig.model_validate_json(path)
-                else:
-                    print(f"Using {available_rigs[choice-1]}.")
-                    rig = AindVrForagingRig.model_validate_json(available_rigs[choice - 1])
+    print(available_rigs)
+    if len(available_rigs) == 1:
+        print(f"Found a single rig config file. Using {available_rigs[0]}.")
+        return load_json_model(available_rigs[0], AindVrForagingRig)
+    else:
+        while True:
+            try:
+                path = pick_file_from_list(available_rigs)
+                rig = load_json_model(path, AindVrForagingRig)
+                print(f"Using {path}.")
                 return rig
-        except ValidationError:
-            print("Failed to validate pydantic model. Try again.")
-        except ValueError:
-            print("Invalid choice. Try again.")
+            except ValidationError:
+                print("Failed to validate pydantic model. Try again.")
+            except ValueError:
+                print("Invalid choice. Try again.")
 
 
 def prompt_bonsai_config_input() -> dict:
@@ -206,7 +212,8 @@ def prompt():
             "SessionPath": save_temp_model(session),
             "RigPath": save_temp_model(rig),
         }
-
+        print()
+        return None
         return run_bonsai_process(
             bonsai_exe=BONSAI_EXE,
             workflow_file=WORKFLOW_FILE,
@@ -221,3 +228,5 @@ def prompt():
 
 if __name__ == "__main__":
     prompt()
+
+
