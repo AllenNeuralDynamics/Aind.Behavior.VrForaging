f18cab40980352027c252c56f9cfb0c69f7c4a01
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-21 17:48:56 -0800

    Add date to Session schema

--- src/DataSchemas/aind_behavior_vr_foraging/session.py
+++ src/DataSchemas/aind_behavior_vr_foraging/session.py
@@ -4,12 +4,14 @@ from typing import Optional, Literal
 # Import aind-datas-schema types
 from aind_data_schema.base import AindCoreModel
 from pydantic import BaseModel, Field
+import datetime
 
 
 class AindVrForagingSession(AindCoreModel):
     describedBy: str = Field("")
     schema_version: Literal["0.1.0"] = "0.1.0"
     experiment: str = Field(..., description="Name of the experiment")
+    date: str = Field(default_factory=datetime.datetime.now, description="Date of the experiment")
     root_path: str = Field(..., description="Root path where data will be logged")
     remote_path: Optional[str] = Field(
         default=None, description="Remote path where data will be attempted to be copied to after experiment is done"
