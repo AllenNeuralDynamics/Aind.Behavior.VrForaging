b9dfc123c779bac917d1ff270323f6e0e52efa4f
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-26 08:47:06 -0800

    Update example

--- src/DataSchemas/examples.py
+++ src/DataSchemas/examples.py
@@ -1,9 +1,11 @@
-import aind_behavior_vr_foraging.distributions as distributions
-import aind_behavior_vr_foraging.rig as rig
-import aind_behavior_vr_foraging.task_logic as task_logic
+import aind_behavior_services.task_logic.distributions as distributions
+import aind_behavior_services.rig as rig
+
+import aind_behavior_vr_foraging.task_logic as vr_task_logic
+
+from aind_behavior_vr_foraging.task_logic import AindVrForagingTaskLogic
 from aind_behavior_vr_foraging.rig import AindVrForagingRig
 from aind_behavior_vr_foraging.session import AindVrForagingSession
-from aind_behavior_vr_foraging.task_logic import AindVrForagingTaskLogic
 
 #  Import the 3 necessary schemas: Rig, Session, and TaskLogic
 
@@ -15,7 +17,7 @@ example_session = AindVrForagingSession(
     remote_path="c://remote",
     subject="test",
     notes="test session",
-    version="0.1.0",
+    experiment_version="0.1.0",
     allow_dirty_repo=True,
     skip_hardware_validation=False,
 )
@@ -44,40 +46,40 @@ example_rig = AindVrForagingRig(
 
 
 def NumericalUpdaterParametersHelper(initial_value, increment, decrement, minimum, maximum):
-    return task_logic.NumericalUpdaterParameters(
+    return vr_task_logic.NumericalUpdaterParameters(
         initial_value=initial_value, increment=increment, decrement=decrement, minimum=minimum, maximum=maximum
     )
 
 
 updaters = {
-    "RewardDelayOffset": task_logic.NumericalUpdater(
-        operation=task_logic.NumericalUpdaterOperation.OFFSET,
+    "RewardDelayOffset": vr_task_logic.NumericalUpdater(
+        operation=vr_task_logic.NumericalUpdaterOperation.OFFSET,
         parameters=NumericalUpdaterParametersHelper(0, 0.005, 0, 0, 0.2),
     ),
-    "StopDurationOffset": task_logic.NumericalUpdater(
-        operation=task_logic.NumericalUpdaterOperation.OFFSET,
+    "StopDurationOffset": vr_task_logic.NumericalUpdater(
+        operation=vr_task_logic.NumericalUpdaterOperation.OFFSET,
         parameters=NumericalUpdaterParametersHelper(0, 0.005, 0, 0, 0.5),
     ),
-    "StopVelocityThreshold": task_logic.NumericalUpdater(
-        operation=task_logic.NumericalUpdaterOperation.OFFSETPERCENTAGE,
+    "StopVelocityThreshold": vr_task_logic.NumericalUpdater(
+        operation=vr_task_logic.NumericalUpdaterOperation.OFFSETPERCENTAGE,
         parameters=NumericalUpdaterParametersHelper(40, 0, -0.25, 10, 40),
     ),
 }
 
 
-operation_control = task_logic.OperationControl(
-    movable_spout_control=task_logic.MovableSpoutControl(
+operation_control = vr_task_logic.OperationControl(
+    movable_spout_control=vr_task_logic.MovableSpoutControl(
         enabled=True,
         time_to_collect_after_reward=1,
-        servo_motor=task_logic.ServoMotor(min_pulse_duration=1000, max_pulse_duration=2000),
+        servo_motor=vr_task_logic.ServoMotor(min_pulse_duration=1000, max_pulse_duration=2000),
     ),
-    audio_control=task_logic.AudioControl(),
-    odor_control=task_logic.OdorControl(
+    audio_control=vr_task_logic.AudioControl(),
+    odor_control=vr_task_logic.OdorControl(
         valve_max_open_time=100000, target_odor_flow=100, target_total_flow=1000, use_channel_3_as_carrier=True
     ),
-    position_control=task_logic.PositionControl(
-        gain=task_logic.Vector3(x=1, y=1, z=1),
-        initial_position=task_logic.Vector3(x=0, y=2.56, z=0),
+    position_control=vr_task_logic.PositionControl(
+        gain=vr_task_logic.Vector3(x=1, y=1, z=1),
+        initial_position=vr_task_logic.Vector3(x=0, y=2.56, z=0),
         frequency_filter_cutoff=5,
         velocity_threshold=40,
     ),
@@ -85,7 +87,7 @@ operation_control = task_logic.OperationControl(
 
 
 def OperantLogicHelper(stop_duration: float = 0.2, is_operant: bool = False):
-    return task_logic.OperantLogic(
+    return vr_task_logic.OperantLogic(
         is_operant=is_operant, stop_duration=stop_duration, time_to_collect_reward=1000000, grace_distance_threshold=10
     )
 
@@ -99,47 +101,47 @@ def ExponentialDistributionHelper(rate: 1, minimum: 0, maximum: 1000):
 
 
 def Reward_VirtualSiteGeneratorHelper(contrast: float = 0.5):
-    return task_logic.VirtualSiteGenerator(
-        render_specification=task_logic.RenderSpecification(contrast=contrast),
-        label=task_logic.VirtualSiteLabels.REWARDSITE,
+    return vr_task_logic.VirtualSiteGenerator(
+        render_specification=vr_task_logic.RenderSpecification(contrast=contrast),
+        label=vr_task_logic.VirtualSiteLabels.REWARDSITE,
         length_distribution=ExponentialDistributionHelper(1, 0, 10),
     )
 
 
 def InterSite_VirtualSiteGeneratorHelper(contrast: float = 0.5):
-    return task_logic.VirtualSiteGenerator(
-        render_specification=task_logic.RenderSpecification(contrast=contrast),
-        label=task_logic.VirtualSiteLabels.INTERSITE,
+    return vr_task_logic.VirtualSiteGenerator(
+        render_specification=vr_task_logic.RenderSpecification(contrast=contrast),
+        label=vr_task_logic.VirtualSiteLabels.INTERSITE,
         length_distribution=ExponentialDistributionHelper(1, 0, 10),
     )
 
 
 def InterPatch_VirtualSiteGeneratorHelper(contrast: float = 1):
-    return task_logic.VirtualSiteGenerator(
-        render_specification=task_logic.RenderSpecification(contrast=contrast),
-        label=task_logic.VirtualSiteLabels.INTERPATCH,
+    return vr_task_logic.VirtualSiteGenerator(
+        render_specification=vr_task_logic.RenderSpecification(contrast=contrast),
+        label=vr_task_logic.VirtualSiteLabels.INTERPATCH,
         length_distribution=ExponentialDistributionHelper(1, 0, 10),
     )
 
 
-reward_function = task_logic.PatchRewardFunction(
-    amount=task_logic.ConstantFunction(value=1),
-    probability=task_logic.ConstantFunction(value=1),
-    available=task_logic.LinearFunction(a=-1, b=5),
-    depletion_rule=task_logic.DepletionRule.ON_CHOICE,
+reward_function = vr_task_logic.PatchRewardFunction(
+    amount=vr_task_logic.ConstantFunction(value=1),
+    probability=vr_task_logic.ConstantFunction(value=1),
+    available=vr_task_logic.LinearFunction(a=-1, b=5),
+    depletion_rule=vr_task_logic.DepletionRule.ON_CHOICE,
 )
 
 
-patch1 = task_logic.PatchStatistics(
+patch1 = vr_task_logic.PatchStatistics(
     label="Amyl Acetate",
     state_index=0,
-    odor_specification=task_logic.OdorSpecification(index=1, concentration=1),
-    reward_specification=task_logic.RewardSpecification(
+    odor_specification=vr_task_logic.OdorSpecification(index=1, concentration=1),
+    reward_specification=vr_task_logic.RewardSpecification(
         reward_function=reward_function,
         operant_logic=OperantLogicHelper(),
         delay=ExponentialDistributionHelper(1, 0, 10),
     ),
-    virtual_site_generation=task_logic.VirtualSiteGeneration(
+    virtual_site_generation=vr_task_logic.VirtualSiteGeneration(
         inter_patch=InterPatch_VirtualSiteGeneratorHelper(),
         inter_site=InterSite_VirtualSiteGeneratorHelper(),
         reward_site=Reward_VirtualSiteGeneratorHelper(),
@@ -147,16 +149,16 @@ patch1 = task_logic.PatchStatistics(
 )
 
 
-patch2 = task_logic.PatchStatistics(
+patch2 = vr_task_logic.PatchStatistics(
     label="Alpha-pinene",
     state_index=1,
-    odor_specification=task_logic.OdorSpecification(index=0, concentration=1),
-    reward_specification=task_logic.RewardSpecification(
+    odor_specification=vr_task_logic.OdorSpecification(index=0, concentration=1),
+    reward_specification=vr_task_logic.RewardSpecification(
         reward_function=reward_function,
         operant_logic=OperantLogicHelper(),
         delay=ExponentialDistributionHelper(1, 0, 10),
     ),
-    virtual_site_generation=task_logic.VirtualSiteGeneration(
+    virtual_site_generation=vr_task_logic.VirtualSiteGeneration(
         inter_patch=InterPatch_VirtualSiteGeneratorHelper(),
         inter_site=InterSite_VirtualSiteGeneratorHelper(),
         reward_site=Reward_VirtualSiteGeneratorHelper(),
@@ -164,21 +166,21 @@ patch2 = task_logic.PatchStatistics(
 )
 
 
-environment_statistics = task_logic.EnvironmentStatistics(
-    first_state=None, transition_matrix=task_logic.Matrix2D(data=[[1, 0], [0, 1]]), patches=[patch1, patch2]
+environment_statistics = vr_task_logic.EnvironmentStatistics(
+    first_state=None, transition_matrix=vr_task_logic.Matrix2D(data=[[1, 0], [0, 1]]), patches=[patch1, patch2]
 )
 
 
-example_task_logic = AindVrForagingTaskLogic(
+example_vr_task_logic = AindVrForagingTaskLogic(
     updaters=updaters,
     environment_statistics=environment_statistics,
-    stage_settings=task_logic.ForagingSettings(),
+    stage_settings=vr_task_logic.ForagingSettings(),
     operation_control=operation_control,
 )
 
-with open("local/example_task_logic.json", "w") as f:
-    f.write(example_task_logic.model_dump_json(indent=3))
+with open("local/example_vr_task_logic.json", "w") as f:
+    f.write(example_vr_task_logic.model_dump_json(indent=2))
 with open("local/example_session.json", "w") as f:
-    f.write(example_session.model_dump_json(indent=3))
+    f.write(example_session.model_dump_json(indent=2))
 with open("local/example_rig.json", "w") as f:
-    f.write(example_rig.model_dump_json(indent=3))
+    f.write(example_rig.model_dump_json(indent=2))
