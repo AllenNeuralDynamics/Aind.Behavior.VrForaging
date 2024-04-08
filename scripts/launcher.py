43594726f27883a39e8881095740d0a8d62576ce
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-08 16:55:17 -0700

    Fix broken reference in launcher

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -1,12 +1,12 @@
 from aind_behavior_services.launcher import LauncherCli
 from aind_behavior_vr_foraging.rig import AindVrForagingRig
-from aind_behavior_vr_foraging.session import AindVrForagingSession
+from aind_behavior_vr_foraging.session import AindBehaviorSessionModel
 from aind_behavior_vr_foraging.task_logic import AindVrForagingTaskLogic
 
 if __name__ == "__main__":
     launcher_cli = LauncherCli(
         rig_schema=AindVrForagingRig,
-        session_schema=AindVrForagingSession,
+        session_schema=AindBehaviorSessionModel,
         task_logic_schema=AindVrForagingTaskLogic,
         data_dir=r"C:/Data",
         remote_data_dir=None,
