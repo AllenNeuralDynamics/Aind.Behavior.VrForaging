520460563fd7267de4290474f115cc4a374fbc8c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-24 08:40:41 -0700

    Add theme

--- docs/conf.py
+++ docs/conf.py
@@ -8,12 +8,19 @@
 
 project = 'vr-foraging-docs'
 copyright = '2024, Allen Institute for Neural Dynamics'
-author = 'Bruno Cru'
+author = 'Bruno Cruz'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinxcontrib.autodoc_pydantic",
+    "sphinx.ext.napoleon",
+    "sphinx_jinja",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +30,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
+html_title = "vr-foraging-docs"
+html_favicon = "_static/favicon.ico"
+html_theme_options = {
+    "light_logo": "light-logo.svg",
+    "dark_logo": "dark-logo.svg",
+}
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+html_show_copyright = False
