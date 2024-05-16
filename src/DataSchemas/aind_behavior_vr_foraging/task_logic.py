7147f2b1c04b5f4948bbc7ecd36c2390a1826b47
brunocruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-05-16 09:51:37 -0700

    Remove `frozen` attribute from `name` field

--- src/DataSchemas/aind_behavior_vr_foraging/task_logic.py
+++ src/DataSchemas/aind_behavior_vr_foraging/task_logic.py
@@ -349,5 +349,5 @@ class AindVrForagingTaskParameters(TaskParameters):
 
 class AindVrForagingTaskLogic(AindBehaviorTaskLogicModel):
     version: Literal[__version__] = __version__
-    name: str = Field(default="AindVrForaging", description="Name of the task logic", frozen=True)
+    name: str = Field(default="AindVrForaging", description="Name of the task logic")
     task_parameters: AindVrForagingTaskParameters = Field(..., description="Parameters of the task logic")
