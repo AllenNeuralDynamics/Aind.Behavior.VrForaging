5cf4ce52169403abda60861dde33115799e44923
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-06-28 10:10:57 -0700

    Linting

--- src/DataSchemas/aind_behavior_vr_foraging/rig.py
+++ src/DataSchemas/aind_behavior_vr_foraging/rig.py
@@ -6,10 +6,17 @@ from typing import Annotated, List, Literal, Optional
 
 import aind_behavior_services.calibration.olfactometer as oc
 import aind_behavior_services.calibration.water_valve as wvc
-from aind_behavior_services.calibration import aind_manipulator
 import aind_behavior_services.rig as rig
-from aind_behavior_services.rig import HarpBehavior, HarpAnalogInput, HarpClockGenerator, HarpLickometer, HarpSniffDetector, Screen
-from aind_behavior_services.rig import AindBehaviorRigModel
+from aind_behavior_services.calibration import aind_manipulator
+from aind_behavior_services.rig import (
+    AindBehaviorRigModel,
+    HarpAnalogInput,
+    HarpBehavior,
+    HarpClockGenerator,
+    HarpLickometer,
+    HarpSniffDetector,
+    Screen,
+)
 from pydantic import BaseModel, Field
 
 __version__ = "0.3.0"
@@ -19,7 +26,8 @@ ValuePair = Annotated[List[float], Field(min_length=2, max_length=2, description
 
 
 class Treadmill(rig.Treadmill):
-    '''Overrides the default settings for the treadmill calibration by spec'ing brake_lookup_calibration field'''
+    """Overrides the default settings for the treadmill calibration by spec'ing brake_lookup_calibration field"""
+
     brake_lookup_calibration: List[ValuePair] = Field(
         default=[[0, 0], [1, 65535]],
         validate_default=True,
@@ -30,27 +38,34 @@ class Treadmill(rig.Treadmill):
 
 
 class AindManipulatorAdditionalSettings(BaseModel):
-    '''Additional settings for the manipulator device'''
+    """Additional settings for the manipulator device"""
+
     spout_axis: aind_manipulator.Axis = Field(default=aind_manipulator.Axis.Y1, description="Spout axis")
 
 
 class AindManipulatorDevice(aind_manipulator.AindManipulatorDevice):
-    '''Overrides the default settings for the manipulator device by spec'ing additional_settings field'''
-    additional_settings: AindManipulatorAdditionalSettings = Field(default=AindManipulatorAdditionalSettings(), description="Additional settings")
+    """Overrides the default settings for the manipulator device by spec'ing additional_settings field"""
+
+    additional_settings: AindManipulatorAdditionalSettings = Field(
+        default=AindManipulatorAdditionalSettings(), description="Additional settings"
+    )
 
 
 class HarpTreadmill(rig.HarpTreadmill):
-    '''Overrides the default settings for the treadmill calibration'''
+    """Overrides the default settings for the treadmill calibration"""
+
     calibration: Treadmill = Field(Treadmill(), description="Treadmill calibration settings", validate_default=True)
 
 
 class HarpOlfactometer(rig.HarpOlfactometer):
-    '''Overrides the default settings for the olfactometer calibration'''
+    """Overrides the default settings for the olfactometer calibration"""
+
     calibration: Optional[oc.OlfactometerCalibration] = Field(default=None, description="Olfactometer calibration")
 
 
 class RigCalibration(BaseModel):
-    '''Container class for calibration models. In a future release these will be moved to the respective devices'''
+    """Container class for calibration models. In a future release these will be moved to the respective devices"""
+
     water_valve: wvc.WaterValveCalibration = Field(..., description="Water valve calibration")
 
 
