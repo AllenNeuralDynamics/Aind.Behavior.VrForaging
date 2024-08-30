cf1061224511222e4488b47d60be8d159791b30e
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-30 13:06:38 -0700

    Linting

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -1,18 +1,17 @@
+import datetime
+from pathlib import Path
+
+from aind_behavior_services.aind_services import watchdog
 from aind_behavior_services.launcher import Launcher
 from aind_behavior_services.session import AindBehaviorSessionModel
 from aind_behavior_vr_foraging.rig import AindVrForagingRig
 from aind_behavior_vr_foraging.task_logic import AindVrForagingTaskLogic
-from aind_behavior_services.aind_services import watchdog
-import datetime
-
-from pathlib import Path
 
 if __name__ == "__main__":
-
     data_dir = Path(r"C:/Data")
     _watchdog = watchdog.Watchdog(
-        project_name="Cognitive flexibility in patch foraging",
-        time_to_run=datetime.time(hour=20))
+        project_name="Cognitive flexibility in patch foraging", schedule_time=datetime.time(hour=20)
+    )
 
     launcher = Launcher(
         rig_schema_model=AindVrForagingRig,
