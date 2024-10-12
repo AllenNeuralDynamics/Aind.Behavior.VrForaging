6a689a853045f8e973adc3d19f570161421a84ee
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-10-12 13:47:30 -0700

    Get services from pipy and regenerate classes

--- src/Extensions/AindVrForagingTaskLogic.cs
+++ src/Extensions/AindVrForagingTaskLogic.cs
@@ -17,7 +17,7 @@ namespace AindVrForagingDataSchema.TaskLogic
     
         private double? _rngSeed;
     
-        private string _aindBehaviorServicesPkgVersion = "0.8.0-rc1";
+        private string _aindBehaviorServicesPkgVersion = "0.8.0";
     
         private System.Collections.Generic.IDictionary<string, NumericalUpdater> _updaters;
     
