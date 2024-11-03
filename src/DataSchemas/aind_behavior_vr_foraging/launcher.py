95cffdcdafb6b46a319069e40bedc62dc9705724
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-11-03 12:29:52 -0800

    Simplify watchdog factory

--- src/DataSchemas/aind_behavior_vr_foraging/launcher.py
+++ src/DataSchemas/aind_behavior_vr_foraging/launcher.py
@@ -1,7 +1,12 @@
+import datetime
+import os
+from functools import partial
 from pathlib import Path
+from typing import Callable, Optional
 
 import aind_behavior_experiment_launcher.launcher.behavior_launcher as behavior_launcher
 from aind_behavior_experiment_launcher.apps.app_service import BonsaiApp
+from aind_behavior_experiment_launcher.data_transfer.watchdog_service import WatchdogDataTransferService
 from aind_behavior_experiment_launcher.resource_monitor.resource_monitor_service import (
     ResourceMonitor,
     available_storage_constraint_factory,
@@ -14,18 +19,56 @@ from aind_behavior_vr_foraging.rig import AindVrForagingRig
 from aind_behavior_vr_foraging.task_logic import AindVrForagingTaskLogic
 
 
+def watchdog_data_transfer_factory(
+    destination: os.PathLike,
+    schedule_time: Optional[datetime.time] = datetime.time(hour=20),
+    project_name: Optional[str] = None,
+    **watchdog_kwargs,
+) -> Callable[[behavior_launcher.BehaviorLauncher], WatchdogDataTransferService]:
+    return partial(
+        _watchdog_data_transfer_factory,
+        destination=destination,
+        schedule_time=schedule_time,
+        project_name=project_name,
+        **watchdog_kwargs,
+    )
+
+
+def _watchdog_data_transfer_factory(
+    launcher: behavior_launcher.BehaviorLauncher, **watchdog_kwargs
+) -> WatchdogDataTransferService:
+    if launcher.services_factory_manager.data_mapper is None:
+        raise ValueError("Data mapper service is not set. Cannot create watchdog.")
+    if not isinstance(launcher.services_factory_manager.data_mapper, AindDataMapperWrapper):
+        raise ValueError(
+            "Data mapper service is not of the correct type (AindDataMapperWrapper). Cannot create watchdog."
+        )
+    if not launcher.services_factory_manager.data_mapper.is_mapped():
+        raise ValueError("Data mapper has not mapped yet. Cannot create watchdog.")
+
+    watchdog = WatchdogDataTransferService(
+        source=launcher.session_directory,
+        aind_session_data_mapper=launcher.services_factory_manager.data_mapper._session_mapper,
+        **watchdog_kwargs,
+    )
+    return watchdog
+
+
 def make_launcher() -> behavior_launcher.BehaviorLauncher:
     data_dir = r"C:/Data"
-    remote_dir = r"\\allen\aind\scratch\vr-foraging\data"
+    remote_dir = Path(r"\\allen\aind\scratch\vr-foraging\data")
     srv = behavior_launcher.BehaviorServicesFactoryManager()
     srv.attach_bonsai_app(BonsaiApp(r"./src/vr-foraging.bonsai"))
     srv.attach_data_mapper(AindDataMapperWrapper.from_launcher)
+    srv.attach_data_transfer(
+        watchdog_data_transfer_factory(remote_dir, project_name="Cognitive flexibility in patch foraging")
+    )
 
     srv.attach_resource_monitor(
         ResourceMonitor(
             constrains=[
                 available_storage_constraint_factory(data_dir, 2e11),
-                remote_dir_exists_constraint_factory(Path(remote_dir).drive),
+                remote_dir_exists_constraint_factory(Path(remote_dir)),
             ]
         )
     )
@@ -37,9 +80,8 @@ def make_launcher() -> behavior_launcher.BehaviorLauncher:
         data_dir=data_dir,
         config_library_dir=r"\\allen\aind\scratch\AindBehavior.db\AindVrForaging",
         temp_dir=r"./local/.temp",
-        repository_dir=None,
-        allow_dirty=False,
-        skip_hardware_validation=False,
+        allow_dirty=True,
+        skip_hardware_validation=True,
         debug_mode=False,
         group_by_subject_log=True,
         services=srv,
