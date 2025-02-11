73d4b7c82e63e0520b624e845b697a2eec4c9218
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-02-10 18:05:21 -0800

    Make olfactometer calibration required

--- src/DataSchemas/aind_behavior_vr_foraging/rig.py
+++ src/DataSchemas/aind_behavior_vr_foraging/rig.py
@@ -39,7 +39,7 @@ class AindManipulatorDevice(aind_manipulator.AindManipulatorDevice):
 class HarpOlfactometer(rig.HarpOlfactometer):
     """Overrides the default settings for the olfactometer calibration"""
 
-    calibration: oc.OlfactometerCalibration = Field(default=None, description="Olfactometer calibration")
+    calibration: oc.OlfactometerCalibration = Field(description="Olfactometer calibration")
 
 
 class RigCalibration(BaseModel):
