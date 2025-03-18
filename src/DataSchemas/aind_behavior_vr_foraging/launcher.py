7c2952fb42178189d4600c036134cddd003425b1
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-03-18 09:09:12 -0700

    Add default video compression settings

--- src/DataSchemas/aind_behavior_vr_foraging/launcher.py
+++ src/DataSchemas/aind_behavior_vr_foraging/launcher.py
@@ -5,10 +5,15 @@ from pathlib import Path
 from typing import Callable, Optional
 
 import aind_behavior_experiment_launcher.launcher.behavior_launcher as behavior_launcher
+import aind_behavior_video_transformation as abvt
 from aind_behavior_experiment_launcher import resource_monitor
 from aind_behavior_experiment_launcher.apps import BonsaiApp
 from aind_behavior_experiment_launcher.data_transfer import aind_watchdog
 from aind_behavior_services.session import AindBehaviorSessionModel
+from aind_watchdog_service.models.manifest_config import (
+    BasicUploadJobConfigs,
+    ModalityConfigs,
+)
 
 from aind_behavior_vr_foraging.data_mappers import AindDataMapperWrapper
 from aind_behavior_vr_foraging.rig import AindVrForagingRig
@@ -22,7 +27,11 @@ def make_launcher() -> behavior_launcher.BehaviorLauncher:
     srv.attach_bonsai_app(BonsaiApp(r"./src/vr-foraging.bonsai"))
     srv.attach_data_mapper(AindDataMapperWrapper.from_launcher)
     srv.attach_data_transfer(
-        watchdog_data_transfer_factory(remote_dir, project_name="Cognitive flexibility in patch foraging")
+        watchdog_data_transfer_factory(
+            remote_dir,
+            project_name="Cognitive flexibility in patch foraging",
+            upload_job_configs=[video_compression_job],
+        )
     )
 
     srv.attach_resource_monitor(
@@ -99,6 +108,23 @@ def _watchdog_data_transfer_factory(
         **watchdog_kwargs,
     )
     return watchdog
+
+
+def video_compression_job(
+    compression_request_settings: Optional[abvt.CompressionRequest] = None,
+) -> Callable[[aind_watchdog.WatchdogDataTransferService], ModalityConfigs]:
+    if compression_request_settings is None:
+        compression_request_settings = abvt.CompressionRequest(compression_enum=abvt.CompressionEnum.GAMMA_ENCODING)
+
+    def _video_compression_job_factory(watchdog: aind_watchdog.WatchdogDataTransferService) -> BasicUploadJobConfigs:
+        return ModalityConfigs(
+            modality=aind_watchdog.Modality.BEHAVIOR_VIDEOS,
+            source=watchdog.source / aind_watchdog.Modality.BEHAVIOR_VIDEOS.abbreviation,
+            job_settings=compression_request_settings,
+            compress_raw_data=True,
+        )
+
+    return _video_compression_job_factory
 
 
 def main():
