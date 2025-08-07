d4a4aa36b0fe67d2c6124f0b9a66e5a3ef93884a
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-08-07 13:30:51 -0700

    Cleanup legacy code from visualizers

--- src/Extensions/SoftwareEventVisualizer.cs
+++ src/Extensions/SoftwareEventVisualizer.cs
@@ -28,7 +28,7 @@ namespace AllenNeuralDynamics.VrForaging
         private static readonly List<ScatterSoftwareEventPlotter> eventPlotters = new List<ScatterSoftwareEventPlotter>()
         {
             new ScatterSoftwareEventPlotter(new SoftwareEventBuffer("GiveReward"), new Vector4(0.12f, 0.56f, 1f, 1), yPoint: 0.45f),
-            new ScatterSoftwareEventPlotter(new SoftwareEventBuffer("Choice"), new Vector4(0.95f, 0.1f, 0.2f, 1), yPoint: 0.55f),
+            new ScatterSoftwareEventPlotter(new SoftwareEventBuffer("ChoiceFeedback"), new Vector4(0.95f, 0.1f, 0.2f, 1), yPoint: 0.55f),
             new ScatterSoftwareEventPlotter(new SoftwareEventBuffer("Lick"), new Vector4(0.05f, 0.05f, 0.05f, 1), marker: null, size: 0.05f, yPoint: 0.5f),
         };
 
