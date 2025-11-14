ea328bcb6d6f926c3e2b8e27c730210f1fb48089
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-11-14 10:51:15 -0800

    Add qc to launcher

--- src/aind_behavior_vr_foraging/launcher.py
+++ src/aind_behavior_vr_foraging/launcher.py
@@ -99,6 +99,24 @@ async def experiment(launcher: Launcher) -> None:
     ads_rig = AindRigDataMapper(rig=rig).map()
     ads_rig.write_standard_file(launcher.session_directory)
 
+    if picker.ui_helper.prompt_yes_no_question("Would you like to run data transfer?"):
+        try:
+            import webbrowser
+
+            from contraqctor.qc.reporters import HtmlReporter
+
+            from .data_qc import make_qc_runner
+
+            vr_dataset = data_contract.dataset(launcher.session_directory)
+            runner = make_qc_runner(vr_dataset)
+            qc_path = launcher.session_directory / "Behavior" / "Logs" / "qc_report.json"
+            reporter = HtmlReporter(output_path=qc_path)
+            runner.run_all_with_progress(reporter=reporter)
+            webbrowser.open(qc_path.as_uri(), new=2)
+        except Exception as e:
+            logger.error(f"Failed to run data QC: {e}")
+
+
     # Watchdog
     is_transfer = picker.ui_helper.prompt_yes_no_question("Would you like to transfer data?")
     if not is_transfer:
