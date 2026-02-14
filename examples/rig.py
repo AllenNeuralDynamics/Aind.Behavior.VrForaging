3b7a9dfd628d2e55a7b9cb5ab0a023cc3e3e188e
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-02-14 12:07:29 -0800

    Update references to aind-behavior-services 0.13 release

--- examples/rig.py
+++ examples/rig.py
@@ -1,34 +1,12 @@
 import datetime
 import os
 
-import aind_behavior_services.rig as rig
-from aind_behavior_services.calibration.aind_manipulator import (
+from aind_behavior_services.rig import cameras, visual_stimulation
+from aind_behavior_services.rig.aind_manipulator import (
     AindManipulatorCalibration,
-    AindManipulatorCalibrationInput,
-    AindManipulatorCalibrationOutput,
     Axis,
     AxisConfiguration,
     ManipulatorPosition,
-)
-from aind_behavior_services.calibration.olfactometer import (
-    Olfactometer,
-    OlfactometerCalibration,
-    OlfactometerCalibrationInput,
-    OlfactometerCalibrationOutput,
-    OlfactometerChannel,
-    OlfactometerChannelConfig,
-    OlfactometerChannelType,
-)
-from aind_behavior_services.calibration.treadmill import (
-    Treadmill,
-    TreadmillCalibration,
-    TreadmillCalibrationInput,
-    TreadmillCalibrationOutput,
-)
-from aind_behavior_services.calibration.water_valve import (
-    Measurement,
-    WaterValveCalibration,
-    WaterValveCalibrationInput,
 )
 from aind_behavior_services.rig.harp import (
     HarpBehavior,
@@ -36,6 +14,18 @@ from aind_behavior_services.rig.harp import (
     HarpSniffDetector,
     HarpWhiteRabbit,
 )
+from aind_behavior_services.rig.olfactometer import (
+    Olfactometer,
+    OlfactometerCalibration,
+    OlfactometerChannel,
+    OlfactometerChannelConfig,
+    OlfactometerChannelType,
+)
+from aind_behavior_services.rig.treadmill import (
+    Treadmill,
+    TreadmillCalibration,
+)
+from aind_behavior_services.rig.water_valve import Measurement, calibrate_water_valves
 
 from aind_behavior_vr_foraging.rig import (
     AindManipulatorDevice,
@@ -44,85 +34,78 @@ from aind_behavior_vr_foraging.rig import (
 )
 
 manipulator_calibration = AindManipulatorCalibration(
-    output=AindManipulatorCalibrationOutput(),
-    input=AindManipulatorCalibrationInput(
-        full_step_to_mm=(ManipulatorPosition(x=0.010, y1=0.010, y2=0.010, z=0.010)),
-        axis_configuration=[
-            AxisConfiguration(axis=Axis.Y1, min_limit=-0.01, max_limit=25),
-            AxisConfiguration(axis=Axis.Y2, min_limit=-0.01, max_limit=25),
-            AxisConfiguration(axis=Axis.X, min_limit=-0.01, max_limit=25),
-            AxisConfiguration(axis=Axis.Z, min_limit=-0.01, max_limit=25),
-        ],
-        homing_order=[Axis.Y1, Axis.Y2, Axis.X, Axis.Z],
-        initial_position=ManipulatorPosition(y1=0, y2=0, x=0, z=0),
-    ),
+    full_step_to_mm=(ManipulatorPosition(x=0.010, y1=0.010, y2=0.010, z=0.010)),
+    axis_configuration=[
+        AxisConfiguration(axis=Axis.Y1, min_limit=-0.01, max_limit=25),
+        AxisConfiguration(axis=Axis.Y2, min_limit=-0.01, max_limit=25),
+        AxisConfiguration(axis=Axis.X, min_limit=-0.01, max_limit=25),
+        AxisConfiguration(axis=Axis.Z, min_limit=-0.01, max_limit=25),
+    ],
+    homing_order=[Axis.Y1, Axis.Y2, Axis.X, Axis.Z],
+    initial_position=ManipulatorPosition(y1=0, y2=0, x=0, z=0),
 )
 
 olfactometer_calibration = OlfactometerCalibration(
-    output=OlfactometerCalibrationOutput(),
     date=datetime.datetime.now(),
-    input=OlfactometerCalibrationInput(
-        channel_config={
-            OlfactometerChannel.Channel0: OlfactometerChannelConfig(
-                channel_index=OlfactometerChannel.Channel0,
-                channel_type=OlfactometerChannelType.ODOR,
-                flow_rate_capacity=100,
-                flow_rate=100,
-                odorant="Amyl Acetate",
-                odorant_dilution=1.5,
-            ),
-            OlfactometerChannel.Channel1: OlfactometerChannelConfig(
-                channel_index=OlfactometerChannel.Channel1,
-                channel_type=OlfactometerChannelType.ODOR,
-                flow_rate_capacity=100,
-                flow_rate=100,
-                odorant="Banana",
-                odorant_dilution=1.5,
-            ),
-            OlfactometerChannel.Channel2: OlfactometerChannelConfig(
-                channel_index=OlfactometerChannel.Channel2,
-                channel_type=OlfactometerChannelType.ODOR,
-                flow_rate_capacity=100,
-                flow_rate=100,
-                odorant="Apple",
-                odorant_dilution=1.5,
-            ),
-            OlfactometerChannel.Channel3: OlfactometerChannelConfig(
-                channel_index=OlfactometerChannel.Channel3,
-                channel_type=OlfactometerChannelType.CARRIER,
-                flow_rate_capacity=1000,
-            ),
-        }
-    ),
+    channel_config={
+        OlfactometerChannel.Channel0: OlfactometerChannelConfig(
+            channel_index=OlfactometerChannel.Channel0,
+            channel_type=OlfactometerChannelType.ODOR,
+            flow_rate_capacity=100,
+            flow_rate=100,
+            odorant="Amyl Acetate",
+            odorant_dilution=1.5,
+        ),
+        OlfactometerChannel.Channel1: OlfactometerChannelConfig(
+            channel_index=OlfactometerChannel.Channel1,
+            channel_type=OlfactometerChannelType.ODOR,
+            flow_rate_capacity=100,
+            flow_rate=100,
+            odorant="Banana",
+            odorant_dilution=1.5,
+        ),
+        OlfactometerChannel.Channel2: OlfactometerChannelConfig(
+            channel_index=OlfactometerChannel.Channel2,
+            channel_type=OlfactometerChannelType.ODOR,
+            flow_rate_capacity=100,
+            flow_rate=100,
+            odorant="Apple",
+            odorant_dilution=1.5,
+        ),
+        OlfactometerChannel.Channel3: OlfactometerChannelConfig(
+            channel_index=OlfactometerChannel.Channel3,
+            channel_type=OlfactometerChannelType.CARRIER,
+            flow_rate_capacity=1000,
+        ),
+    },
 )
 
-water_valve_input = WaterValveCalibrationInput(
+water_valve_calibration = calibrate_water_valves(
     measurements=[
         Measurement(valve_open_interval=0.2, valve_open_time=0.01, water_weight=[0.6, 0.6], repeat_count=200),
         Measurement(valve_open_interval=0.2, valve_open_time=0.02, water_weight=[1.2, 1.2], repeat_count=200),
     ]
 )
-water_valve_calibration = WaterValveCalibration(
-    input=water_valve_input, output=water_valve_input.calibrate_output(), date=datetime.datetime.now()
-)
 
-video_writer = rig.cameras.VideoWriterFfmpeg(frame_rate=120, container_extension="mp4")
+video_writer = cameras.VideoWriterFfmpeg(frame_rate=120, container_extension="mp4")
 
 rig = AindVrForagingRig(
+    data_directory=r"C:/data",
+    computer_name="TestRigComputer",
     rig_name="test_rig",
-    triggered_camera_controller=rig.cameras.CameraController[rig.cameras.SpinnakerCamera](
+    triggered_camera_controller=cameras.CameraController[cameras.SpinnakerCamera](
         frame_rate=120,
         cameras={
-            "FaceCamera": rig.cameras.SpinnakerCamera(
+            "FaceCamera": cameras.SpinnakerCamera(
                 serial_number="SerialNumber", binning=1, exposure=5000, gain=0, video_writer=video_writer
             ),
-            "SideCamera": rig.cameras.SpinnakerCamera(
+            "SideCamera": cameras.SpinnakerCamera(
                 serial_number="SerialNumber", binning=1, exposure=5000, gain=0, video_writer=video_writer
             ),
         },
     ),
-    monitoring_camera_controller=rig.cameras.CameraController[rig.cameras.WebCamera](
-        cameras={"WebCam0": rig.cameras.WebCamera(index=0)}
+    monitoring_camera_controller=cameras.CameraController[cameras.WebCamera](
+        cameras={"WebCam0": cameras.WebCamera(index=0)}
     ),
     harp_behavior=HarpBehavior(port_name="COM3"),
     harp_olfactometer=Olfactometer(port_name="COM4", calibration=olfactometer_calibration),
@@ -133,14 +116,11 @@ rig = AindVrForagingRig(
     harp_treadmill=Treadmill(
         port_name="COM8",
         calibration=TreadmillCalibration(
-            input=TreadmillCalibrationInput(),
-            output=TreadmillCalibrationOutput(
-                wheel_diameter=15, pulses_per_revolution=28800, brake_lookup_calibration=[[0, 0], [1, 65535]]
-            ),
+            wheel_diameter=15, pulses_per_revolution=28800, brake_lookup_calibration=[[0, 0], [1, 65535]]
         ),
     ),
     manipulator=AindManipulatorDevice(port_name="COM9", calibration=manipulator_calibration),
-    screen=rig.visual_stimulation.Screen(display_index=1),
+    screen=visual_stimulation.ScreenAssembly(display_index=1),
     calibration=RigCalibration(water_valve=water_valve_calibration),
 )
 
