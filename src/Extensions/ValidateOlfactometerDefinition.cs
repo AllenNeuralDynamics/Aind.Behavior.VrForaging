ac3558267cd997223c5187965f663de19605206c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-04-20 13:56:48 -0700

    Further validate the total flow at startup

--- src/Extensions/ValidateOlfactometerDefinition.cs
+++ src/Extensions/ValidateOlfactometerDefinition.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using AindVrForagingDataSchema;
 using Bonsai;
 
@@ -10,12 +11,16 @@ using Bonsai;
 [WorkflowElementCategory(ElementCategory.Combinator)]
 public class ValidateOlfactometerDefinition
 {
+    const int minimumCarrierFlow = 100;
+
     public IObservable<bool> Process(IObservable<Tuple<AindVrForagingTaskLogic, AindVrForagingRig>> source)
     {
         return source.Select(value =>
         {
             var taskLogic = value.Item1;
             var rig = value.Item2;
+            var perOdorFlow = taskLogic.TaskParameters.OperationControl.OdorControl.TargetOdorFlow;
+            int maxTotalFlow = taskLogic.TaskParameters.OperationControl.OdorControl.TargetTotalFlow;
             var nChannels = 3 + 4 * rig.HarpOlfactometerExtension.Count; // 3 for the main olfactometer, 4 for each additional olfactometer
             var patches = taskLogic.TaskParameters.Environment.Blocks.SelectMany(block => block.EnvironmentStatistics.Patches);
             int maxIdx = 0;
@@ -28,6 +33,11 @@ public class ValidateOlfactometerDefinition
                         maxIdx = Math.Max(maxIdx, i);
                         break;
                     }
+                }
+
+                if ((patch.OdorSpecification.Sum() * perOdorFlow) > (maxTotalFlow - minimumCarrierFlow))
+                {
+                    throw new InvalidOperationException("The olfactometer definition in the task logic requires a total flow of " + (patch.OdorSpecification.Sum() * perOdorFlow) + " ml/min, but the maximum total flow is " + (maxTotalFlow - minimumCarrierFlow) + " ml/min.");
                 }
             }
             if (maxIdx >= nChannels)
