bb03058f63c92fd918ba856e7b25f30779dab81e
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-06-08 11:54:42 -0700

    Launch with terminal

--- scripts/launcher.cmd
+++ scripts/launcher.cmd
@@ -1,7 +1,13 @@
 @echo off
 setlocal
 set "scriptPath=%~dp0"
+set "repoPath=%scriptPath%.."
 set "launcherPath=%scriptPath%launcher.ps1"
-powershell -ExecutionPolicy Bypass -File "%launcherPath%"
+where wt.exe >nul 2>&1
+if %errorlevel% == 0 (
+    wt.exe -d "%repoPath%" powershell -ExecutionPolicy Bypass -File "%launcherPath%"
+) else (
+    powershell -ExecutionPolicy Bypass -File "%launcherPath%"
+)
 endlocal
 exit
